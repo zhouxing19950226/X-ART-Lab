a9fbf73b4ccad15de58f9528829911a5a20c387c
zhouxing19950226 <103412333+zhouxing19950226@users.noreply.github.com>
2026-08-22 01:16:13 +0200

    恢复原始 Logo 并缩小主屏幕图标

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "x-art-lab-v7";
-const APP_SHELL = ["/", "/manifest.webmanifest?v=3", "/icons/icon-192.png?v=3", "/icons/icon-512.png?v=3"];
+const CACHE_NAME = "x-art-lab-v8";
+const APP_SHELL = ["/", "/manifest.webmanifest?v=4", "/icons/icon-192.png?v=4", "/icons/icon-512.png?v=4"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
